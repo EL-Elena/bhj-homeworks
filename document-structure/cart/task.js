e5610e45d0385818dc8d699715984e52a1ce3c17
Elena Emelianova <elnall@mail.ru>
2023-05-22 13:30:26 +0800

    Задание готово

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -4,7 +4,10 @@ const cart = document.querySelector('.cart__products');
 
 chooseQantity.forEach((i) => {
   i.onclick = () => {
-    if(i.classList.contains('product__quantity-control_dec') && Number(i.nextElementSibling.innerText) != 0) {  
+    if(i.classList.contains('product__quantity-control_dec')) {  
+       if(Number(i.nextElementSibling.innerText) == 1) {
+          return;
+       }
        i.nextElementSibling.innerText = Number(i.nextElementSibling.innerText) - 1;
     } else if (i.classList.contains('product__quantity-control_inc')){
         i.previousElementSibling.innerText = Number(i.previousElementSibling.innerText) + 1; 
@@ -13,29 +16,18 @@ chooseQantity.forEach((i) => {
 })
 
 addToCart.forEach((i) => {
-  i.onclick = () => {
-    let allId = [];
-      Array.from(document.querySelectorAll('.cart__product')).forEach((j) => { 
-        allId.push(j.getAttribute('data-id'));
-      });
-
-    if(allId.includes(i.closest('.product').getAttribute('data-id'))) {
+  i.onclick = () => { 
+    let allProductsInCart = Array.from(document.querySelectorAll('.cart__product'));
+    if(allProductsInCart.find((j) => j.getAttribute('data-id') == i.closest('.product').getAttribute('data-id'))) {
        cart.querySelector(`div[data-id="${i.closest('.product').getAttribute('data-id')}"]`).querySelector('.cart__product-count').innerText = Number(i.previousElementSibling.querySelector('.product__quantity-value').innerText) + Number(cart.querySelector(`div[data-id="${i.closest('.product').getAttribute('data-id')}"]`).querySelector('.cart__product-count').innerText);
        return;
-    }
+    };
 
-    if(i.previousElementSibling.querySelector('.product__quantity-value').innerText == 0) {
-       return; 
-    } else {
        cart.insertAdjacentHTML('beforeend', `
         <div class="cart__product" data-id="${i.closest('.product').getAttribute('data-id')}">
           <img class="cart__product-image" src="${i.closest('.product').querySelector('.product__image').getAttribute('src')}">
           <div class="cart__product-count">${i.previousElementSibling.querySelector('.product__quantity-value').innerText}</div>
         </div>
        `);
-      }
-  }  
+  }     
 })
-
-
-
--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,25 +1,23 @@
 const tasksList = document.querySelector('#tasks__list');
 const input = document.querySelector('#task__input');
 
-input.addEventListener('keypress', (i) => {
-  if(i.code == 'Enter' && input.value != '') {
-     tasksList.insertAdjacentHTML('beforeend', 
-       `<div class="task">
-         <div class="task__title">
-           ${input.value}
-         </div>
-         <a href="#" class="task__remove">&times;</a>
-       </div>`
-     )
-
-     Array.from(document.querySelectorAll('.task__remove')).forEach((j) => {
-       j.onclick = () => {
-         j.closest('.task').remove();
-       }
-    })
-
-     input.value = '';
-     i.preventDefault();
+window.addEventListener('click', (i) => {
+  if(i.target.classList.contains('tasks__add') && input.value.trim() != '') {
+    tasksList.insertAdjacentHTML('beforeend', 
+      `<div class="task">
+        <div class="task__title">
+          ${input.value}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+      </div>`
+    );
+    input.value = '';
+  };
+  
+  if(i.target.classList.contains('task__remove')) {
+     i.target.closest('.task').remove();
   }
+
+  i.preventDefault();
 })
 
